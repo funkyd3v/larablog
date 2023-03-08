76b9b9d139e0789e748c89c06108f57e4f52ea79
funkyd3v <sdiu.cse215@gmail.com>
2023-03-08 16:30:11 +0600

    redirect route change to blog route

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -26,7 +26,7 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::BLOG;
 
     /**
      * Create a new controller instance.
