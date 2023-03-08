43185a3a8b237002f31e6bb4453dd713063fa6dd
funkyd3v <sdiu.cse215@gmail.com>
2023-03-08 22:27:06 +0600

    api created

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const BLOG = '/blog';
+    public const HOME = '/blog';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
